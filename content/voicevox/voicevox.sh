b9017f804bbc250035813c879fc86667bc6828cb
ancient-blog <t.uehata@gmail.com>
2023-09-10 08:17:31 +0900

    refs #17 スクリプトでログのコンソール出力

--- content/voicevox/voicevox.sh
+++ content/voicevox/voicevox.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
-for filename in $(ls ./content/voicevox/*.txt)
+for filename in ./content/voicevox/*.txt
 do
+    echo "Processing $filename..."
 
-#jsonファイルの作成
-curl -s \
-    -X POST \
-    "localhost:50021/audio_query?speaker=1"\
-    --get --data-urlencode text@$filename \
-    > query.json
+    # jsonファイルの作成
+    curl -s \
+        -X POST \
+        "localhost:50021/audio_query?speaker=1" \
+        --get --data-urlencode text@"$filename" \
+        > query.json
 
-#wavファイルの作成
-curl -s \
-    -H "Content-Type: application/json" \
-    -X POST \
-    -d @query.json \
-    "localhost:50021/synthesis?speaker=1" \
-    > audio.wav
-    
-#wavファイルのリネーム
-mv audio.wav "${filename}.wav"
+    echo "Created query.json for $filename"
 
+    # JSONファイルの内容を表示
+    echo "JSON Response:"
+    cat query.json
+
+    # wavファイルの作成
+    curl -s \
+        -H "Content-Type: application/json" \
+        -X POST \
+        -d @query.json \
+        "localhost:50021/synthesis?speaker=1" \
+        > audio.wav
+
+    echo "Created audio.wav for $filename"
+
+    # 音声ファイルの作成結果を表示
+    echo "Audio Response:"
+    cat audio.wav
+
+    # wavファイルのリネーム
+    mv audio.wav "${filename}.wav"
+
+    echo "Renamed audio.wav to ${filename}.wav"
+
+    echo "Finished processing $filename"
 done
